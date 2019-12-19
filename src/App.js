03a9dbafe84c40800e9cff0ae59620160003c47d
Mr. Jonah <jaitch@gmail.com>
2019-12-19 12:21:22 -0600

    Dropped GO effect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Create React App</h1>
       <h2>
         Deployed with{' '}
         <a
